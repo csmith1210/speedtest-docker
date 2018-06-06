fa03d39d37bfa361017d2f604c446a91887ee312
Connor Smith <csmith1095@gmail.com>
2018-06-06 11:59:34 -0400

    Update run.sh to just use environmental variables

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# SHORT_OUTPUT: set to true to force SpeedTest script into text output mode
-# SHARE: set to true to get a results URL printed to the docker logs as well
-# SERVER: set this variable to a specific server URL if you want, otherwise leave empty
-SHORT_OUTPUT=true;
-SHARE=true;
-SERVER="ohcn.OST.myvzw.com:8080";
-
 cmd="/usr/local/bin/SpeedTest";
 if [ "$SHORT_OUTPUT" = "true" ]; then
    cmd="$cmd --output text";
