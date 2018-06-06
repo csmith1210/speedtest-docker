ce70c43411d13c505c7369ac7cd3d926a00deccb
Connor Smith <csmith1095@gmail.com>
2018-06-06 11:22:30 -0400

    Add ability to set server specifically

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-printf "\nSpeed Test $(date +'%D %T')\n"
+# SHORT_OUTPUT: set to true to force SpeedTest script into text output mode
+# SHARE: set to true to get a results URL printed to the docker logs as well
+# SERVER: set this variable to a specific server URL if you want, otherwise leave empty
+SHORT_OUTPUT=true;
+SHARE=true;
+SERVER="ohcn.OST.myvzw.com:8080";
 
-/usr/local/bin/SpeedTest --output text --share
+cmd="/usr/local/bin/SpeedTest";
+if [ "$SHORT_OUTPUT" = "true" ]; then
+   cmd="$cmd --output text";
+fi
+if [ "$SHARE" = "true" ]; then
+   cmd="$cmd --share";
+fi
+if [ "$SERVER" != "" ]; then
+   cmd="$cmd --test-server $SERVER";
+fi
+
+printf "\nSpeed Test $(date +'%D %T')\n"
+eval $cmd
